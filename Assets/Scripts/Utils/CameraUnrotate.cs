e71b420b905f17a469c4ff2531f3ad65000fcee5
Nicolas Agustín Perez <nicolasperez95.a@gmail.com>
2022-07-26 17:57:33 -0300

    feat: added collectables

--- Assets/Scripts/Utils/CameraUnrotate.cs
+++ Assets/Scripts/Utils/CameraUnrotate.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraUnrotate : MonoBehaviour
-{
-    private Transform transform = null;
-    private Quaternion originalRotation;
-    private void Start()
+namespace Utils { 
+    public class CameraUnrotate : MonoBehaviour
     {
-        transform = GetComponent<Transform>();
-        originalRotation = transform.rotation;
-    }
+        private Transform transform = null;
+        private Quaternion originalRotation;
+        private void Awake()
+        {
+            transform = GetComponent<Transform>();
+            originalRotation = transform.rotation;
+        }
 
-    void Update()
-    {
-        transform.rotation = originalRotation;
+        void Update()
+        {
+            transform.rotation = originalRotation;
+        }
     }
 }
