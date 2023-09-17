0df16ede166524e2126965b5dbcb1d6550bc4c3c
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2023-09-17 03:45:55 -0300

    feat: improved on web

--- Assets/Scripts/Utils/CameraFollower.cs
+++ Assets/Scripts/Utils/CameraFollower.cs
@@ -22,8 +22,12 @@ namespace Utils {
             targetPostion.y = (followY) ? target.position.y : transform.position.y;
 
 
-            transform.position = Vector2.MoveTowards(transform.position, targetPostion + offset, Time.deltaTime * 100);
+            transform.position = Vector2.MoveTowards(transform.position, targetPostion + offset, Time.deltaTime * speed * 100);
         }
     }
 }
 
+/** 
+ * <iframe frameborder="0" src="https://itch.io/embed/1474485?linkback=true&amp;border_width=0&amp;bg_color=000000&amp;fg_color=ffffff&amp;link_color=f43a3a&amp;border_color=000000" width="550" height="165"><a href="https://nperez9.itch.io/super-cannon-blast">Super Cannon blast! by Nicolas Agustin</a></iframe>
+ * 
+ * **/
